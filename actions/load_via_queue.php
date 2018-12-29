7ec455357fcd1c27f7bb111b7e3b31a18fbf9f6f
Roderic Page <rdmpage@gmail.com>
2018-12-29 11:46:52 +0000

    More examples

--- actions/load_via_queue.php
+++ actions/load_via_queue.php
@@ -5,7 +5,7 @@
 require_once(dirname(dirname(__FILE__)) . '/queue/queue.php');
 
 $force = false;
-$force = true;
+//$force = true;
 
 $urls = array();
 
@@ -13,6 +13,27 @@ $urls = array('https://doi.org/10.3969/j.issn.1000-3142.2007.06.001');
 
 $urls = array('https://doi.org/10.6165/tai.1983.28.146');
 
+$urls = array(
+'https://doi.org/10.1663/0007-196X(2003)055[0205:DRZANS]2.0.CO;2',
+'https://doi.org/10.1017/S096042860000192X',
+);
+
+$urls = array(
+'http://www.siamese-heritage.org/nhbsspdf/vol041-050/NHBSS_049_1k_Larsen_ANewSpeciesOfDisti.pdf'
+);
+
+$urls = array(
+'http://www.siamese-heritage.org/nhbsspdf/vol041-050/NHBSS_049_1k_Larsen_ANewSpeciesOfDisti.pdf'
+);
+
+$urls = array(
+'http://www.jjbotany.com/getpdf.php?aid=10761'
+);
+
+$urls = array(
+'https://doi.org/10.6165/tai.2008.53(3).248',
+'http://old.ssbg.asu.ru/turcz/turcz_14_3_14-27.pdf'
+);
 
 // Add items to the queue
 foreach ($urls as $url)
