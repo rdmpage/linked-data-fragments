f006a3ec39239711fe7fcca02d5a2410f7d8db38
Roderic Page <rdmpage@gmail.com>
2019-07-28 10:03:18 +1000

    Lots of stuff

--- resolvers/csl2jsonld.php
+++ resolvers/csl2jsonld.php
@@ -409,7 +409,7 @@ function csl_to_json($obj)
 
 
 // test
-if (0)
+if (1)
 {
 
 
@@ -457,15 +457,294 @@ if (0)
 		"volume": "11",
 		"_id": "https://pub.orcid.org/v2.1/0000-0002-0876-3286/work/16657720"
 	}';
+	
+	
+	$json = '{
+    "status": "ok",
+    "message-type": "work",
+    "message-version": "1.0.0",
+    "message": {
+        "indexed": {
+            "date-parts": [
+                [
+                    2019,
+                    2,
+                    13
+                ]
+            ],
+            "date-time": "2019-02-13T00:59:41Z",
+            "timestamp": 1550019581191
+        },
+        "reference-count": 14,
+        "publisher": "Pensoft Publishers",
+        "license": [
+            {
+                "URL": "http://creativecommons.org/licenses/by/3.0/",
+                "start": {
+                    "date-parts": [
+                        [
+                            2013,
+                            3,
+                            21
+                        ]
+                    ],
+                    "date-time": "2013-03-21T00:00:00Z",
+                    "timestamp": 1363824000000
+                },
+                "delay-in-days": 0,
+                "content-version": "vor"
+            }
+        ],
+        "content-domain": {
+            "domain": [],
+            "crossmark-restriction": false
+        },
+        "short-container-title": [
+            "ZK"
+        ],
+        "DOI": "10.3897/zookeys.278.4765",
+        "type": "journal-article",
+        "created": {
+            "date-parts": [
+                [
+                    2013,
+                    3,
+                    21
+                ]
+            ],
+            "date-time": "2013-03-21T14:17:26Z",
+            "timestamp": 1363875446000
+        },
+        "page": "75-90",
+        "source": "Crossref",
+        "is-referenced-by-count": 0,
+        "title": [
+            "Four new species of Unixenus Jones, 1944 (Diplopoda, Penicillata, Polyxenida) from Australia"
+        ],
+        "prefix": "10.3897",
+        "volume": "278",
+        "author": [
+            {
+                "given": "Megan",
+                "family": "Short",
+                "sequence": "first",
+                "affiliation": []
+            },
+            {
+                "given": "Cuong",
+                "family": "Huynh",
+                "sequence": "additional",
+                "affiliation": []
+            }
+        ],
+        "member": "2258",
+        "published-online": {
+            "date-parts": [
+                [
+                    2013,
+                    3,
+                    21
+                ]
+            ]
+        },
+        "reference": [
+            {
+                "key": "3746_B1",
+                "author": "Burt",
+                "year": "1984",
+                "journal-title": "Report on the research of the pincushion millipede, Unixenus mjoebergi (Verhoeff, 1924) at Tom Price in the Pilbara of Western Australia."
+            },
+            {
+                "issue": "4",
+                "key": "3746_B2",
+                "first-page": "254",
+                "article-title": "Dipolopodes Pénicillates de Madagascar et des Mascareignes",
+                "volume": "29",
+                "author": "Condé",
+                "year": "1962",
+                "journal-title": "Revue Française d’Entomologie"
+            },
+            {
+                "key": "3746_B3",
+                "author": "Condé",
+                "year": "1963",
+                "journal-title": "Pénicillates de Côte d’Ivoire (récoltes de M. Vuillaume). Bulletin Scientifique de l’Institut Fondamental d’Afrique Noire 25(A): 669–684."
+            },
+            {
+                "key": "3746_B4",
+                "doi-asserted-by": "crossref",
+                "first-page": "47",
+                "DOI": "10.5962/bhl.part.81868",
+                "article-title": "Diplopodes Pénicillates de Papouasie et de Bornéo",
+                "volume": "91",
+                "author": "Condé",
+                "year": "1984",
+                "journal-title": "Revue Suisse Zoologie"
+            },
+            {
+                "issue": "4",
+                "key": "3746_B5",
+                "first-page": "291",
+                "article-title": "Classification actuelle des Diplopodes Pénicillates (Myriapodes) avec nouvelles définitions des taxa",
+                "volume": "133",
+                "author": "Condé",
+                "year": "2008",
+                "journal-title": "Bulletin de la Société zoologique de France"
+            },
+            {
+                "key": "3746_B6",
+                "first-page": "138",
+                "article-title": "On two new south Indian pselaphognathous diplopods",
+                "volume": "119",
+                "author": "Jones",
+                "year": "1937",
+                "journal-title": "Zoologischer Anzeiger"
+            },
+            {
+                "issue": "3",
+                "key": "3746_B7",
+                "first-page": "94",
+                "article-title": "Mechanism of defence in a pselaphognathous diplopod, Unixenus padmanabhii Jones",
+                "volume": "31",
+                "author": "Jones",
+                "year": "1944",
+                "journal-title": "Proceedings of the Indian Science Congress"
+            },
+            {
+                "key": "3746_B8",
+                "author": "Koch",
+                "year": "1985",
+                "journal-title": "Pincushion millipedes (Diplopoda: Polyxenida): Their aggregations and identity in Western Australia. The Western Australian Naturalist 16(2/3): 30–32."
+            },
+            {
+                "key": "3746_B9",
+                "doi-asserted-by": "crossref",
+                "first-page": "153",
+                "DOI": "10.1111/j.1096-3642.1957.tb02516.x",
+                "article-title": "The Evolution of Arthropodan Locomotory Mechanisms – Part 5: The Structure, Habits and Evolution of the Pselaphognatha (Diplopoda)",
+                "volume": "43",
+                "author": "Manton",
+                "year": "1956",
+                "journal-title": "Journal of the Linnean Society London"
+            },
+            {
+                "key": "3746_B10",
+                "first-page": "43",
+                "article-title": "",
+                "volume": "64",
+                "author": "Nguyen",
+                "year": "1967",
+                "journal-title": "Mitteilungen aus dem Hamburgischen Zoologischen Museum und Institut"
+            },
+            {
+                "key": "3746_B11",
+                "author": "Nguyen",
+                "year": "1982",
+                "journal-title": "Lophoproctidés insulaire de l’océan Pacifique (Diplopodes Pénicillates). Bulletin du Muséum d’Histoire Naturelle de Paris, 4e Série, Section A (1-2): 95–118."
+            },
+            {
+                "key": "3746_B12",
+                "doi-asserted-by": "crossref",
+                "first-page": "105",
+                "DOI": "10.3897/zookeys.156.2168",
+                "article-title": "The genus Unixenus Jones, 1944(Diplopoda, Penicillata, Polyxenida) in Australia",
+                "volume": "156",
+                "author": "Short",
+                "year": "2011",
+                "journal-title": "Zookeys"
+            },
+            {
+                "key": "3746_B13",
+                "first-page": "214",
+                "article-title": "Tavola sinottica dei generi dei Diplopoda Penicillata",
+                "volume": "8",
+                "author": "Silvestri",
+                "year": "1948",
+                "journal-title": "Bollettino del Laboratorio di Entomologia Agraria, Portici"
+            },
+            {
+                "issue": "5",
+                "key": "3746_B14",
+                "first-page": "1",
+                "article-title": "Results of Dr. E. Mjöbergi’s Swedish Scientific Expeditions to Australia 1910–1913. 34. Myriapoda: Diplopoda",
+                "volume": "16",
+                "author": "Verhoeff",
+                "year": "1924",
+                "journal-title": "Arkiv för Zoologi"
+            }
+        ],
+        "container-title": [
+            "ZooKeys"
+        ],
+        "original-title": [],
+        "link": [
+            {
+                "URL": "http://zookeys.pensoft.net/article_preview.php?id=3746&skip_redirect=1",
+                "content-type": "unspecified",
+                "content-version": "vor",
+                "intended-application": "similarity-checking"
+            }
+        ],
+        "deposited": {
+            "date-parts": [
+                [
+                    2017,
+                    6,
+                    21
+                ]
+            ],
+            "date-time": "2017-06-21T09:47:34Z",
+            "timestamp": 1498038454000
+        },
+        "score": 1,
+        "subtitle": [],
+        "short-title": [],
+        "issued": {
+            "date-parts": [
+                [
+                    2013,
+                    3,
+                    21
+                ]
+            ]
+        },
+        "references-count": 14,
+        "URL": "http://dx.doi.org/10.3897/zookeys.278.4765",
+        "relation": {
+            "cites": []
+        },
+        "ISSN": [
+            "1313-2970",
+            "1313-2989"
+        ],
+        "issn-type": [
+            {
+                "value": "1313-2989",
+                "type": "print"
+            },
+            {
+                "value": "1313-2970",
+                "type": "electronic"
+            }
+        ]
+    }
+}';
 
 	$obj = json_decode($json);
 	
-	$jsonld = csl_to_json($obj);
+	if (isset($obj->message))
+	{
+		$jsonld = csl_to_json($obj->message);
+	}
+	else
+	{	
+		$jsonld = csl_to_json($obj);
+	}
 
 	//print_r($jsonld);
 
-	//echo json_encode($jsonld, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-	//echo "\n";
+	echo json_encode($jsonld, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+	echo "\n";
 }
 
 
